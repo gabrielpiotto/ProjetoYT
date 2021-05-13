a5ba9d3fe3ae86c97771e2d218dc54597b43e593
Gabriel Piotto <gabrielpiottogdm@gmail.com>
2021-05-13 11:28:59 -0300

    Iniciando o repositorio com a classe pessoa

---
+++ ProjetoYT/src/Pessoa.java
@@ -0,0 +1,57 @@
+
+public class Pessoa {
+
+    // Atributos da classe:
+    protected String nome;
+    protected String sexo;
+    protected int idade;
+    protected int experiencia;
+
+    // Métodos:
+    /**
+     * Adicionar experiencia
+     */
+    public void ganharExp() {
+
+    }
+
+    /**
+     * Construtor
+     */
+    public void Pessoa() {
+
+    }
+
+    // Getters e Setters:
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSexo() {
+        return this.sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getExperiencia() {
+        return this.experiencia;
+    }
+
+    public void setExperiencia(int experiencia) {
+        this.experiencia = experiencia;
+    }
+}
